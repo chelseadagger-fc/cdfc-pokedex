9d7b43a859202e508136391af24e026f04507585
Daniel <daniel.aldridge@alumni.unc.edu>
2023-07-24 23:32:30 +0300

    created useState for expanding/collapsing search component on mobile

--- components/PokeList.tsx
+++ components/PokeList.tsx
@@ -1,14 +1,20 @@
+'use client'
+
+import React, { useCallback } from 'react';
 import Search from './Search'
 
 export default function PokeList() {
+    
+    const [isExpanded, setIsExpanded] = React.useState(false);
+    const toggleIsExpanded = useCallback(() => {
+        setIsExpanded((isExpanded) => !isExpanded);
+    }, []);
+    
     return (
         <div className="flex flex-col-reverse h-full lg:h-screen lg:flex-col lg:justify-start">
             <div className="bg-zinc-400">
                 <Search />
             </div>
-            {/* <div className="h-full">
-                <h1>PokeList</h1>
-            </div> */}
         </div>
     )
 }
